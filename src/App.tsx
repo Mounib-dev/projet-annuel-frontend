a04dee120af7eb6931c0d1b82784fa103f6afe0b
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-02-16 18:35:37 +0100

    Adding global layout component to wrap the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import "./assets/App.css";
 import { Route, Routes } from "react-router-dom";
-import NavBar from "./components/layout/NavBar";
+import Layout from "./components/layout/Layout";
 import Home from "./components/Home";
-import Register from "./components/Register";
-import Login from "./components/Login";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
-import Transaction from "./components/Transaction";
+import Transaction from "./components/transaction/Transaction";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/Transaction" element={<Transaction />}></Route>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Transaction" element={<Transaction />}></Route>
+        </Routes>
+      </Layout>
     </>
   );
 }
