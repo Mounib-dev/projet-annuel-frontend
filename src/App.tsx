8ab0f6be8318671440ee43db21eb615afba898d4
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-03-09 13:37:19 +0100

    Wiring up everything on the main app

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,32 @@ import Login from "./components/auth/Login";
 
 import Transaction from "./components/transaction/Transaction";
 
+import DialogModal from "./components/utils/DialogModal";
+
+import { AuthProvider } from "./context/AuthContext";
+import PrivateRoute from "./components/auth/PrivateRoute";
+
+import ChatBot from "./components/chatbot/ChatBot";
+
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/Transaction" element={<Transaction />}></Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/transaction" element={<Transaction />}></Route>
+            <Route
+              path="/test"
+              element={<DialogModal isOpen={false} children={undefined} />}
+            ></Route>
+            <Route path="/ai-assistant" element={<ChatBot />} />
+          </Route>
         </Routes>
       </Layout>
-    </>
+    </AuthProvider>
   );
 }
 
